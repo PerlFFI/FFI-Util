308def95df2421cfd98cd312ffcda43c0cb31943
Graham Ollis <perl@wdlabs.com>
2013-11-25 11:40:35 -0500

    rename dereference to deref

--- lib/FFI/Util.pm
+++ lib/FFI/Util.pm
@@ -25,19 +25,19 @@ ffi_lib do {
   \$file;
 };
 
-attach_function "dereference_to_ptr",    [ _ptr ], _ptr;
-attach_function "dereference_to_int",    [ _ptr ], _int;
-attach_function "dereference_to_uint",   [ _ptr ], _uint;
-attach_function "dereference_to_short",  [ _ptr ], _short;
-attach_function "dereference_to_ushort", [ _ptr ], _ushort;
-attach_function "dereference_to_char",   [ _ptr ], _char;
-attach_function "dereference_to_uchar",  [ _ptr ], _uchar;
-attach_function "dereference_to_float",  [ _ptr ], _float;
-attach_function "dereference_to_double", [ _ptr ], _double;
-attach_function "dereference_to_int64",  [ _ptr ], _int64;
-attach_function "dereference_to_uint64", [ _ptr ], _uint64;
+attach_function "deref_to_ptr",    [ _ptr ], _ptr;
+attach_function "deref_to_int",    [ _ptr ], _int;
+attach_function "deref_to_uint",   [ _ptr ], _uint;
+attach_function "deref_to_short",  [ _ptr ], _short;
+attach_function "deref_to_ushort", [ _ptr ], _ushort;
+attach_function "deref_to_char",   [ _ptr ], _char;
+attach_function "deref_to_uchar",  [ _ptr ], _uchar;
+attach_function "deref_to_float",  [ _ptr ], _float;
+attach_function "deref_to_double", [ _ptr ], _double;
+attach_function "deref_to_int64",  [ _ptr ], _int64;
+attach_function "deref_to_uint64", [ _ptr ], _uint64;
 
-our @EXPORT_OK = map { "dereference_to_$_" } qw( ptr int uint short ushort char uchar float double int64 uint64 );
+our @EXPORT_OK = map { "deref_to_$_" } qw( ptr int uint short ushort char uchar float double int64 uint64 );
 
 sub scalar_to_buffer
 {
--- xs/dereference.c
+++ xs/dereference.c
@@ -1,56 +1,56 @@
 #include <stdint.h>
 
-void *dereference_to_ptr(void **ptr)
+void *deref_to_ptr(void **ptr)
 {
   return *ptr;
 }
 
-int dereference_to_int(int *ptr)
+int deref_to_int(int *ptr)
 {
   return *ptr;
 }
 
-unsigned int dereference_to_uint(unsigned int *ptr)
+unsigned int deref_to_uint(unsigned int *ptr)
 {
   return *ptr;
 }
 
-short dereference_to_short(short *ptr)
+short deref_to_short(short *ptr)
 {
   return *ptr;
 }
 
-unsigned short dereference_to_ushort(unsigned short *ptr)
+unsigned short deref_to_ushort(unsigned short *ptr)
 {
   return *ptr;
 }
 
-char dereference_to_char(char *ptr)
+char deref_to_char(char *ptr)
 {
   return *ptr;
 }
 
-unsigned char dereference_to_uchar(unsigned char *ptr)
+unsigned char deref_to_uchar(unsigned char *ptr)
 {
   return *ptr;
 }
 
-float dereference_to_float(float *ptr)
+float deref_to_float(float *ptr)
 {
   return *ptr;
 }
 
-double dereference_to_double(double *ptr)
+double deref_to_double(double *ptr)
 {
   return *ptr;
 }
 
-int64_t dereference_to_int64(int64_t *ptr)
+int64_t deref_to_int64(int64_t *ptr)
 {
   return *ptr;
 }
 
-uint64_t dereference_to_uint64(uint64_t *ptr)
+uint64_t deref_to_uint64(uint64_t *ptr)
 {
   return *ptr;
 }
